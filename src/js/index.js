640056880c70e698d57f7024c48a94cac4cfc4e7
Jasintu <128087894+Jasintu@users.noreply.github.com>
2023-09-07 01:32:53 -0300

    UP

--- src/js/index.js
+++ src/js/index.js
@@ -66,7 +66,10 @@ function savanna() {
     bottle.innerText = "SAVANNA";
     h4.innerHTML = "The Earth’s area affected by desertification is approx 11 times India’s size.";
     h3.innerHTML = "The Savannas act as a carbon sink, absorbing CO2 from the atmosphere and helping to maintain the balance of global temperatures";
-    img.src = "https://cdn.shopify.com/s/files/1/0689/1443/t/34/assets/savanna_OK.png?v=4783820813181844557";
+    img.src = "images/savanna.png";
+    img.style.width = "240px";
+    img.style.height = "490px";
+    img.style.marginLeft = "0px";
     imgBackground.style.background = "url(https://images.unsplash.com/photo-1613109526778-27605f1f27d2?ixlib=rb-1…xMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2340&q=80)";
     imgBackground.style.backgroundSize = "cover";
 }
@@ -75,7 +78,10 @@ function glacier() {
     bottle.innerText = "GLACIER";
     h4.innerHTML = "Glaciers contain 75% of the World's freshwater.";
     h3.innerHTML = "The effects of melting ice glaciers are biodiversity loss, the rising of the sea level and the deficiency of freshwater, among others.";
-    img.src = "https://cdn.shopify.com/s/files/1/0689/1443/t/34/assets/Glacier_OK.png?v=7185877315400411030";
+    img.src = "images/glaicer.png";
+    img.style.width = "255px";
+    img.style.height = "440px";
+    img.style.marginLeft = "0px";
     imgBackground.style.background = "url(https://www.discover-the-world.com/app/uploads/2018/05/chile-patagonia-calving-glacier-is-1150x863-c-default.jpg)";
     imgBackground.style.backgroundSize = "cover";
 }
@@ -84,7 +90,10 @@ function coral() {
     bottle.innerText = "CORAL";
     h4.innerHTML = "We will have lost 60% of our coral reefs by 2030.";
     h3.innerHTML = "Coral reefs are essential to humans, as they protect the shorelines and are a source of nutrients and habitat for thousands of marine species.";
-    img.src = "https://cdn.shopify.com/s/files/1/0689/1443/t/34/assets/Coral_OK.png?v=145969";
+    img.src = "images/coral.png";
+    img.style.width = "245px";
+    img.style.height = "480px";
+    img.style.marginLeft = "-10px";
     imgBackground.style.background = "url(https://images.unsplash.com/photo-1546500840-ae38253aba9b?ixid=MnwxMjA3…90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3260&q=80)";
     imgBackground.style.backgroundSize = "cover";
 }
@@ -93,7 +102,10 @@ function beach() {
     bottle.innerText = "BEACH";
     h4.innerHTML = "In 20 years, there could be more plastic in our oceans than fish.";
     h3.innerHTML = "Plastic pollution injures more than 100.000 marine animals every year.It takes around 450 years for one plastic bottle to decompos.";
-    img.src = "https://cdn.shopify.com/s/files/1/0689/1443/t/34/assets/bottle_beach.png?v=1178426";
+    img.src = "images/beach.png";
+    img.style.width = "300px";
+    img.style.height = "500px";
+    img.style.marginLeft = "0px";
     imgBackground.style.background = "url(https://cdn.shopify.com/s/files/1/0689/1443/t/34/assets/Beach_1920.jpg?v=1029210661698833530)";
     imgBackground.style.backgroundSize = "cover";
 }
